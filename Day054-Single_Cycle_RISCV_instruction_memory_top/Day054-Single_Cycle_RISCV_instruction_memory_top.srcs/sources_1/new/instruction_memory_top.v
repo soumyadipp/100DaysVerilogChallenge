e1c9ea5995e6d63e48353e71dca90eb0030dfe7e
Soumyadip <52304110+soumyadipp@users.noreply.github.com>
2025-03-25 23:36:29 +0530

    Add files via upload

---
+++ Day054-Single_Cycle_RISCV_instruction_memory_top/Day054-Single_Cycle_RISCV_instruction_memory_top.srcs/sources_1/new/instruction_memory_top.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: SOUMYADIP MANNA
+// 
+// Create Date: 25.03.2025 23:23:37
+// Design Name: RISCV
+// Module Name: instruction_memory_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instruction_memory_top #(
+    parameter INIT_MEM = 0
+)(
+    input wire clk,
+    input wire rst,
+    input wire we_re,
+    input wire request,
+    input wire [3:0]  mask,
+    input wire [7:0]  address,
+    input wire [31:0] data_in,
+
+    output reg valid,
+    output wire [31:0] data_out
+    );
+
+    always @(posedge clk or negedge rst ) begin
+        if(!rst)begin
+            valid <= 0;
+        end
+        else begin
+            valid <= request;
+        end
+    end
+
+    instruction_memory #(
+      .INIT_MEM(INIT_MEM)
+    )u_memory(
+        .clk(clk),
+        .we_re(we_re),
+        .request(request),
+        .mask(mask),
+        .address(address),
+        .data_in(data_in),
+        .data_out(data_out)
+    );
+endmodule
